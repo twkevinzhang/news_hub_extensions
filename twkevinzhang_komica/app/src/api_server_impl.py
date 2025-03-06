deb39a7fdd68c4e4a8cace5067ce07f2c9460a7c
twkevinzhang <twkevinzhang@gmail.com>
2025-03-06 09:03:00 +0800

    fix(twkevinzhang_komica): pagination

--- twkevinzhang_komica/app/src/api_server_impl.py
+++ twkevinzhang_komica/app/src/api_server_impl.py
@@ -12,6 +12,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
 
+
 def get(url: str):
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
@@ -25,6 +26,21 @@ def get(url: str):
     logging.debug(f"{url} Encoding: {encoding}")
     html_content = response.content.decode(encoding, errors='ignore')
     return html_content
+
+
+def pagination(req: pb2.PaginationReq, items) -> (list, pb2.PaginationRes):
+    page_size = req.page_size if req.page_size is not None and req.page_size > 0 else 20  # default page size is 20
+    page = req.page if req.page is not None and req.page > 0 else 1  # default page is 1
+    start_index = (page - 1) * page_size
+    end_index = min(start_index + page_size, len(items))
+    total_page = (len(items) + page_size - 1) // page_size
+    return (
+        items[start_index:end_index],
+        pb2.PaginationRes(
+            current_page=req.page,
+            total_page=total_page,
+        ))
+
 
 class ApiServerImpl(pb2_grpc.ExtensionApiServicer):
     def __init__(self):
@@ -42,6 +58,10 @@ class ApiServerImpl(pb2_grpc.ExtensionApiServicer):
         )
 
     def GetBoards(self, req: pb2.GetBoardsReq, context) -> pb2.GetBoardsRes:
+        boards, page = parse_boards.list(), None
+        if req.page is not None:
+            boards, page = pagination(req.page, boards)
+
         return pb2.GetBoardsRes(
             boards=[
                 pb2.Board(
@@ -52,8 +72,9 @@ class ApiServerImpl(pb2_grpc.ExtensionApiServicer):
                     large_welcome_image=x.large_welcome_image,
                     url=x.url,
                     supported_threads_sorting=x.supported_threads_sorting,
-                ) for x in parse_boards.list()
-            ]
+                ) for x in boards
+            ],
+            page=page,
         )
 
     def GetThreadInfos(self, req: pb2.GetThreadInfosReq, context) -> pb2.GetThreadInfosRes:
@@ -61,13 +82,14 @@ class ApiServerImpl(pb2_grpc.ExtensionApiServicer):
         board_id = salt.decode(req.board_id)
         [subdomain, board_sub_id] = board_id.split("/")
         response = get(f'https://{subdomain}.komica1.org/{board_sub_id}/index.htm')
-        thread_infos = parse_thread_infos_html(response, site_id, board_id)
+
+        thread_infos, page = parse_thread_infos_html(response, site_id, board_id), None
+        if req.page is not None:
+            thread_infos, page = pagination(req.page, thread_infos)
+
         return pb2.GetThreadInfosRes(
-            thread_infos=[ thread.toSaltPb2() for thread in thread_infos ],
-            page=pb2.PaginationRes(
-                current_page=1,
-                total_page=1,
-            )
+            thread_infos=[thread.toSaltPb2() for thread in thread_infos],
+            page=page,
         )
 
     def GetThreadPost(self, req: pb2.GetThreadPostReq, context) -> pb2.GetThreadPostRes:
@@ -77,6 +99,7 @@ class ApiServerImpl(pb2_grpc.ExtensionApiServicer):
         post_id = salt.decode(req.post_id)
         [subdomain, board_sub_id] = board_id.split("/")
         response = get(f'https://{subdomain}.komica1.org/{board_sub_id}/pixmicat.php?res={thread_id}')
+
         thread = parse_thread_html(response, site_id, board_id, thread_id, post_id)
         return pb2.GetThreadPostRes(
             thread_post=thread.toSaltPb2(),
@@ -89,9 +112,14 @@ class ApiServerImpl(pb2_grpc.ExtensionApiServicer):
         post_id = salt.decode(req.post_id)
         [subdomain, board_sub_id] = board_id.split("/")
         response = get(f'https://{subdomain}.komica1.org/{board_sub_id}/pixmicat.php?res={thread_id}')
-        posts = parse_regarding_posts_html(response, site_id, board_id, thread_id, post_id)
+
+        posts, page = parse_regarding_posts_html(response, site_id, board_id, thread_id, post_id), None
+        if req.page is not None:
+            posts, page = pagination(req.page, posts)
+
         return pb2.GetRegardingPostsRes(
-            regarding_posts=[ post.toSaltPb2() for post in posts ],
+            regarding_posts=[post.toSaltPb2() for post in posts],
+            page=page,
         )
 
     def GetComments(self, req: pb2.GetCommentsReq, context) -> pb2.GetThreadInfosRes:
